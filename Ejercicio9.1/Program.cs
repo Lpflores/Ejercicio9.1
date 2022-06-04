e8eae928e5f70706391bd4b025eb7580d4a1fd7f
Usuario <Usuario@DESKTOP-FUD0TVD>
2022-06-04 16:44:28 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio9.1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("Ingrese la longitud deseada del vector: ");
+var dato = Console.ReadLine();
+Console.WriteLine();
+int n  = int.Parse(dato);
+int[] vector = new int[n];
+Random aleatorio = new Random();
+
+
+for (int i = 0; i < vector.Length; i++)
+{
+   
+    vector[i] = aleatorio.Next();
+
+}
+Console.WriteLine("El vector generado es:");
+foreach (int valor in vector)
+{
+    Console.WriteLine(valor + " ");
+
+}
+
+for (int i = 0; i < vector.Length / 2; i++)
+{
+    int j = vector.Length - i - 1;
+    int invertido = vector[j];
+    vector[j] = vector[i];
+    vector[i] = invertido;
+
+}
+
+Console.WriteLine("El vector invertido es: ");
+    foreach(int valor in vector)
+{
+    Console.WriteLine(valor + " ");
+
+}
+
+
+
